907415a3a975fd88ef42c3c9cbe378699026fe32
lleonor <leahaprilleonor@gmail.com>
2022-02-04 09:20:11 -0600

    Added new component server at server folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { ServerComponent } from './server/server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule
