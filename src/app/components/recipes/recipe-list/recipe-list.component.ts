42ed4325d4a1ffa5359d7fa9ee3be739947619ed
lleonor <leahaprilleonor@gmail.com>
2022-02-04 12:47:45 -0600

    Added display layout

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Adobo', 'Favorite filipino recipe' , 'https://cdn-icons-png.flaticon.com/512/2005/2005938.png'),
+    new Recipe('Menudo', 'Favorite filipino recipe1' , 'https://cdn-icons-png.flaticon.com/512/2005/2005938.png'),
+    new Recipe('Afritada', 'Favorite filipino recipe2' , 'https://cdn-icons-png.flaticon.com/512/2005/2005938.png'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
 
   constructor() { }
 
